b99f60ee7379157bd4ed890f8701ac91a13d8e06
sanjai06 <nsksanjai@gmail.com>
2024-01-22 09:16:00 +0530

    new build

---
+++ .next/server/chunks/8722.js
@@ -0,0 +1,75 @@
+"use strict";
+exports.id = 8722;
+exports.ids = [8722];
+exports.modules = {
+
+/***/ 8722:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+// import "./styles.css";
+function Pagination({ prev , currentPage , getPaginationGroup , next , pages , handleActive ,  }) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                className: "pagination justify-content-start",
+                children: [
+                    getPaginationGroup.length <= 0 ? null : /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                        onClick: prev,
+                        className: "page-item",
+                        children: currentPage === 1 ? null : /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            className: "page-link",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                                className: "fi-rs-angle-double-small-left"
+                            })
+                        })
+                    }),
+                    getPaginationGroup.map((item, index)=>{
+                        return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                            onClick: ()=>handleActive(item)
+                            ,
+                            className: currentPage === item ? "page-item active" : "page-item",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                className: "page-link",
+                                children: item
+                            })
+                        }, index);
+                    }),
+                    getPaginationGroup.length <= 0 ? null : /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                        onClick: next,
+                        className: "page-item",
+                        children: currentPage >= pages ? null : /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            className: "page-link",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                                className: "fi-rs-angle-double-small-right"
+                            })
+                        })
+                    })
+                ]
+            }),
+            getPaginationGroup.length <= 0 ? null : /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    "show ",
+                    currentPage,
+                    " of ",
+                    pages
+                ]
+            })
+        ]
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Pagination);
+
+
+/***/ })
+
+};
+;
